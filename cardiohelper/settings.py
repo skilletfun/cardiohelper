b112236c8c5f744ed2df0db2e73e3f077bf6e45d
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-09-20 22:34:52 +0300

    Fix staticfiles

--- cardiohelper/settings.py
+++ cardiohelper/settings.py
@@ -86,7 +86,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_ROOT = ''
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = 'media/'
 
